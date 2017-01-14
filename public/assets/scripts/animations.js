861b73c752d4000f0d60aa50fd3330d5ab2e89d5
Fernando <fcoelho@hugeinc.com>
2017-01-13 23:01:18 -0200

    issue #000 | Add messages to the first page

--- public/assets/scripts/animations.js
+++ public/assets/scripts/animations.js
@@ -1,11 +1,58 @@
 (function () {
     'use strict';
     
-    let name = document.querySelector('.name'),
+    var name = document.querySelector('.name'),
         header = document.querySelector('.header'),
-        arrow = document.querySelector('.arrow');
+        arrow = document.querySelector('.arrow'),
+        main = document.querySelector('.main'),
+        lastScrollTop = 0;
 
-    name.addEventListener('click', function () {
+    name.addEventListener('click', () => {
        header.classList.toggle('-active');
     });
+
+    // document.addEventListener('scroll', function(event) {
+    //     let scrollPosition = window.pageYOffset;
+    //     var $elm = $(event.target);
+    //
+    //     if( $elm.is('.main')){ // or any other filtering condition
+    //         // do some stuff
+    //         if (scrollPosition > lastScrollTop) {
+    //             header.classList.add('-scroll-up');
+    //             console.log('scrolling2');
+    //         } else {
+    //             header.classList.remove('-scroll-up');
+    //             console.log('scrolling');
+    //         }
+    //         lastScrollTop = scrollPosition;
+    //     }
+    // }.bind(this));
+    //
+    // $(".main").scroll(function() { //.box is the class of the div
+    //     var scrollPosition = main.scrollTop;
+    //     console.log(scrollPosition )
+    //     if (scrollPosition > lastScrollTop) {
+    //         header.classList.add('-scroll-up');
+    //         console.log('scrolling2');
+    //     } else {
+    //         header.classList.remove('-scroll-up');
+    //         console.log('scrolling');
+    //     }
+    //     lastScrollTop = scrollPosition;
+    //
+    //
+    // }.bind(this));
+
+
+    $('.main').on('scroll', function () {
+        let scrollPosition = main.scrollTop;
+        if (scrollPosition > lastScrollTop) {
+            header.classList.add('-scroll-up');
+        } else {
+            header.classList.remove('-scroll-up');
+        }
+        lastScrollTop = scrollPosition;
+
+    }.bind(this));
+    //
 })();
