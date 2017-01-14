26f96e2f3dc66ed5d0631d2870ae63bd5998de39
Fernando <fcoelho@hugeinc.com>
2017-01-14 16:36:46 -0200

    issue #000 | Add receipt info

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -24,6 +24,21 @@
         }
     }
 
+    var os = require('os');
+
+    var interfaces = os.networkInterfaces();
+    var addresses = [];
+    for (var k in interfaces) {
+        for (var k2 in interfaces[k]) {
+            var address = interfaces[k][k2];
+            if (address.family === 'IPv4' && !address.internal) {
+                addresses.push(address.address);
+            }
+        }
+    }
+
+    console.log(addresses);
+
     /***
      * connectToSocketIO
      */
