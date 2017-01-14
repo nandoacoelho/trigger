3f155c0f03817c641afc201c40f219d2bd509ebd
Fernando <fcoelho@hugeinc.com>
2017-01-14 10:25:47 -0200

    issue #000 | Add new anonymous page

--- public/assets/scripts/animations.js
+++ public/assets/scripts/animations.js
@@ -11,51 +11,64 @@
         blackOverlay = document.querySelector('.black-overlay'),
         user = document.querySelector('.user'),
         back = document.querySelector('.user-icon.back'),
-
-
+        userIconUser = document.querySelector('.user-icon.user'),
         main = document.querySelector('.main'),
+        messages = document.querySelector('.messages'),
         profile = document.querySelector('.profile'),
         profileInfo = document.querySelector('.profile-info'),
-
+        microphone = document.querySelector('.microphone'),
+        paperPlane = document.querySelector('.paper-plane'),
         lastScrollTop = 0;
 
     name.addEventListener('click', () => {
         blackOverlay.classList.toggle('-active');
         overlay.classList.toggle('-active');
-        console.log('blau')
+        header.classList.remove('-scroll-up');
         header.classList.toggle('-active');
         profile.classList.toggle('-active');
-        body.classList.toggle('-no-overflow');
+        messages.classList.toggle('-no-overflow');
         name.classList.toggle('-active');
         user.classList.toggle('-active');
-
         profileInfo.classList.toggle('-active');
+        userIconUser.classList.toggle('-active');
     });
 
     back.addEventListener('click', () => {
         blackOverlay.classList.toggle('-active');
         overlay.classList.toggle('-active');
-        console.log('blau')
         header.classList.toggle('-active');
         profile.classList.toggle('-active');
-        body.classList.toggle('-no-overflow');
+        messages.classList.toggle('-no-overflow');
         name.classList.toggle('-active');
         user.classList.toggle('-active');
-
+        header.classList.remove('-scroll-up');
         profileInfo.classList.toggle('-active');
+        userIconUser.classList.toggle('-active');
     });
 
-    $('.messages').on('scroll', function () {
-        let scrollPosition = main.scrollTop;
+    $('.messages').on('scroll', () => {
+        let scrollPosition = messages.scrollTop;
         if (scrollPosition > lastScrollTop) {
             header.classList.add('-scroll-up');
-        } else {
+            messages.classList.add('-scroll-up');
+        }
+        else {
             header.classList.remove('-scroll-up');
+            messages.classList.remove('-scroll-up');
         }
         lastScrollTop = scrollPosition;
-    }.bind(this));
+    });
 
-    // var d = $('.messages');
-    // d.scrollTop(d.prop("scrollHeight"));
-    $(".messages").animate({ scrollTop: $('.messages').prop("scrollHeight")}, 1000);
+    $(".messages").animate({ scrollTop: $('.messages').prop("scrollHeight")},0);
+    header.classList.remove('-scroll-up');
+
+    $('.textarea').on('focusin',function(){
+        microphone.classList.remove('-active');
+        paperPlane.classList.add('-active');
+    });
+
+    $('.textarea').on('focusout',function(){
+        microphone.classList.add('-active');
+        paperPlane.classList.remove('-active');
+    });
 })();
